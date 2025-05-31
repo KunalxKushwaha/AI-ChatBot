c1c70a3e5af5ae9af82e7b5e7a2cb3ac34766863
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-05-31 17:14:14 +0530

    Backend Started

---
+++ script.js
@@ -0,0 +1,22 @@
+const API_KEY = "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";
+const fetchData =
+     fetch("https://openrouter.ai/api/v1/chat/completions", {
+  method: "POST",
+  headers: {
+    "Authorization": "Bearer <OPENROUTER_API_KEY>",
+    "HTTP-Referer": "<YOUR_SITE_URL>", // Optional. Site URL for rankings on openrouter.ai.
+    "X-Title": "<YOUR_SITE_NAME>", // Optional. Site title for rankings on openrouter.ai.
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+    "model": "deepseek/deepseek-r1-distill-llama-70b:free",
+    "messages": [
+      {
+        "role": "user",
+        "content": "What is the meaning of life?"
+      }
+    ]
+  })
+});
+
+

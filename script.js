61b82e8fca472ea76645aa9d1c79cd34293feec4
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-05-31 17:58:58 +0530

    Backend Started

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 const API_KEY = "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";
 
+const content = document.getElementById("content");
 const chatInp = document.getElementById("Chat-Input");
 const sendButton = document.getElementById("Send-Button");
 sendButton.addEventListener('click', () => handlesendmessage());
+chatInp.addEventListener('keypress', (event) =>{
+  if (event.key === 'Enter') {
+    handlesendmessage();
+  }
+}); 
 
 function handlesendmessage() {
       const question = chatInp.value.trim();
       console.log("Question:", question);
+      addQuestionSection(question);
     }
 
 const fetchData =
      fetch("https://openrouter.ai/api/v1/chat/completions", {
   method: "POST",
   headers: {
-    "Authorization": `Bearer ${API_KEY}`, // Replace with your OpenRouter API key
-    "HTTP-Referer": "<YOUR_SITE_URL>", // Optional. Site URL for rankings on openrouter.ai.
-    "X-Title": "<YOUR_SITE_NAME>", // Optional. Site title for rankings on openrouter.ai.
+    "Authorization": `Bearer ${API_KEY}`, 
     "Content-Type": "application/json"
   },
   body: JSON.stringify({
@@ -38,3 +43,15 @@ const fetchData =
 //     console.error("Error:", error);
 //   });
 
+function addQuestionSection(messsage) {
+    const SectionElement = document.createElement("section");
+    SectionElement.className = "Ques-Section";
+    SectionElement.textContent = messsage;
+
+    content.appendChild(SectionElement);
+    addAnswerSection(messsage);
+}
+
+function addAnswerSection(messsage) {
+  
+}
